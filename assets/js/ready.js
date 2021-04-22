0ac55865adb489a13bcdbe645a2c74c8512d4366
Billiam <billiamthesecond@gmail.com>
2021-04-21 21:58:17 -0500

    Fix double initialization

--- assets/js/ready.js
+++ assets/js/ready.js
@@ -1,8 +1,3 @@
 export default function (fn) {
-  if (document.readyState != 'loading'){
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
   document.documentElement.addEventListener('turbo:load', fn)
 }
